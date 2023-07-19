e609ce5bb896dd1cdae028a6a4f50618e42d8db9
Dylan Walls <dylanwalls@Dylans-MacBook-Pro-2.local>
2023-07-19 21:27:35 +0200

    first

---
+++ create-pdf-from-docx.js
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2019 Adobe
+ * All Rights Reserved.
+ *
+ * NOTICE: Adobe permits you to use, modify, and distribute this file in
+ * accordance with the terms of the Adobe license agreement accompanying
+ * it. If you have received this file from a source other than Adobe,
+ * then your use, modification, or distribution of it requires the prior
+ * written permission of Adobe.
+ */
+
+const PDFServicesSdk = require('@adobe/pdfservices-node-sdk');
+const fs = require('fs');
+const INPUT = '/Users/dylanwalls/Documents/Bitprop/partnership_agreements/PDFServicesSDK-Node.jsSamples/adobe-dc-pdf-services-sdk-node/resources/test.docx';
+const OUTPUT = './testesave.pdf';
+const JSON_INPUT = {
+    "name": "Dylan",
+    "surname": "Walls"
+}
+
+/**
+ * This sample illustrates how to create a PDF file from a DOCX file.
+ * <p>
+ * Refer to README.md for instructions on how to run the samples.
+ */
+try {
+    // Initial setup, create credentials instance.
+    const credentials =  PDFServicesSdk.Credentials
+        .servicePrincipalCredentialsBuilder()
+        .withClientId("c8e4cd5828dd4a73bad876a83e3714a4")
+        .withClientSecret("p8e-4FwcEry59N3aTEvx52Io2z6C4krJCKEK")
+        .build();
+
+    // Create an ExecutionContext using credentials and create a new operation instance.
+    const executionContext = PDFServicesSdk.ExecutionContext.create(credentials);
+        // createPdfOperation = PDFServicesSdk.CreatePDF.Operation.createNew();
+
+    const documentMerge = PDFServicesSdk.DocumentMerge,
+            documentMergeOptions = documentMerge.options,
+            options = new documentMergeOptions.DocumentMergeOptions(JSON_INPUT, documentMergeOptions.OutputFormat.PDF);
+ 
+    // Create a new operation instance using the options instance.
+    const documentMergeOperation = documentMerge.Operation.createNew(options);
+    
+    // Set operation input document template from a source file.
+    const input = PDFServicesSdk.FileRef.createFromLocalFile(INPUT);
+    documentMergeOperation.setInput(input);
+ 
+    // Execute the operation and Save the result to the specified location.
+    documentMergeOperation.execute(executionContext)
+    .then(result => result.saveAsFile(OUTPUT))
+    .catch(err => {
+        if(err instanceof PDFServicesSdk.Error.ServiceApiError
+            || err instanceof PDFServicesSdk.Error.ServiceUsageError) {
+            console.log('Exception encountered while executing operation', err);
+        } else {
+            console.log('Exception encountered while executing operation', err);
+        }
+    });
+
+
+    
+    // // Set operation input from a source file.
+    
+    // createPdfOperation.setInput(input);
+
+    // //Generating a file name
+    // let outputFilePath = createOutputFilePath();
+
+    // // Execute the operation and Save the result to the specified location.
+    // createPdfOperation.execute(executionContext)
+    //     .then(result => result.saveAsFile(outputFilePath))
+    //     .catch(err => {
+    //         if(err instanceof PDFServicesSdk.Error.ServiceApiError
+    //             || err instanceof PDFServicesSdk.Error.ServiceUsageError) {
+    //             console.log('Exception encountered while executing operation', err);
+    //         } else {
+    //             console.log('Exception encountered while executing operation', err);
+    //         }
+    //     });
+
+    // //Generates a string containing a directory structure and file name for the output file.
+    // function createOutputFilePath() {
+    //     let date = new Date();
+    //     let dateString = date.getFullYear() + "-" + ("0" + (date.getMonth() + 1)).slice(-2) + "-" +
+    //         ("0" + date.getDate()).slice(-2) + "T" + ("0" + date.getHours()).slice(-2) + "-" +
+    //         ("0" + date.getMinutes()).slice(-2) + "-" + ("0" + date.getSeconds()).slice(-2);
+    //     // return ("output/CreatePDFFromDOCX/create" + dateString + ".pdf");
+    //     return ("testsave.pdf");
+    // }
+
+} catch (err) {
+    console.log('Exception encountered while executing operation', err);
+}
